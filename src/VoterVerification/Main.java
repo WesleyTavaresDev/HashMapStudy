2dde5bf8ac849b75c39f0cbbbfdc9f5a6d0c3c1e
Wesley <wesleyTavaresDev@outlook.com>
2022-01-25 08:25:22 -0300

    VoterCheck

---
+++ src/VoterVerification/Main.java
@@ -0,0 +1,30 @@
+package VoterVerification;
+
+import java.util.HashMap;
+import java.lang.Boolean;
+
+public class Main
+{
+    HashMap<String, Boolean> voted = new HashMap<String, Boolean>();
+
+    public static void main(String[] args)
+    {
+        Main mainClass = new Main();
+
+        mainClass.VoterCheck("Bob");
+        mainClass.VoterCheck("Pam");
+        mainClass.VoterCheck("Bob");
+    }
+
+    public void VoterCheck(String name)
+    {
+
+        if(voted.get(name) != null)
+            System.out.printf("%s has already voted%n", name);
+        else
+        {
+            voted.put(name, true);
+            System.out.printf("%s can vote %n", name);
+        }
+    }
+}
